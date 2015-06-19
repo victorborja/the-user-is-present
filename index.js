3a13d98f97da23998bacc072ea1197da5a61fb49
Victor Borja <vborja@apache.org>
2015-06-19 00:38:30 -0500

    add pages

--- index.js
+++ index.js
@@ -1,20 +1,58 @@
 +(function ($) {
 
-  setTimeout(marinaStatic, 4000);
+  page('/marina', show('marina'), marina);
+  page('/:user', show('user'), noop);
+  page('/', show('welcome'), noop);
+
+  page('*', function () {
+    page.redirect('/')
+  })
+
+  page({
+    hashbang: true
+  });
+
+  $(document).on('click', '[data-start]', openCamera)
+
+  function noop () {}
+
+  function show (pageName) {
+    return function (ctx, next) {
+      console.log('hide', $('section:not(.'+pageName+')'));
+      console.log('show', $('section.'+pageName));
+      $('section:not(.'+pageName+')').hide();
+      $('section.'+pageName).show();
+      next();
+    }
+  }
+
+  function openCamera () {
+    var check = setInterval(proceed, 500);
+    Webcam.attach('#my_camera');
+
+    function proceed() {
+      if (Webcam.video) {
+        clearInterval(check);
+        page('/marina');
+      }
+    }
+  }
+
+  function marina () {
+    console.log("Marina");
+    setTimeout(marinaStatic, 4000);
+  }
 
   function marinaStatic() {
     $('.marina.anim').removeClass('anim');
   }
 
-  Webcam.attach( '#my_camera' );
 
   function take_snapshot() {
     Webcam.snap( function(data_uri) {
       document.getElementById('my_result').innerHTML = '<img src="'+data_uri+'"/>';
-    } );
+    });
   }
-
-  
 
 
 }(jQuery));
